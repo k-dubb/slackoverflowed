f1efb6454e977bbf4bc54bacbd3fab898575bed2
levi koskan <levikoskan@gmail.com>
2016-01-20 11:50:40 -0600

    added 5 posts to home page for the loggin user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :questions
   devise_for :users
   resources :questions
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

9390aff5a4d2ff320dafc19977be2d9fd5461689
david anguiano <davidanguianojr@gmail.com>
2016-01-21 02:37:50 -0600

    added 'searchable' to questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,24 @@
 class Question < ActiveRecord::Base
   belongs_to :user
 
+  searchable do
+    text :title, :body
+    text :comments do
+      comments.map { |comment| comment.body }
+    end
+
+    boolean :featured
+    integer :blog_id
+    integer :author_id
+    integer :category_ids, :multiple => true
+    double  :average_rating
+    time    :published_at
+    time    :expired_at
+
+    string  :sort_title do
+      title.downcase.gsub(/^(an?|the)/, '')
+    end
+  end
+end
+
 end

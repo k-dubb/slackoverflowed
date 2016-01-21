e18f9a0ee19418785beb9bc4cb44d48de6e58608
david anguiano <davidanguianojr@gmail.com>
2016-01-21 02:52:54 -0600

    added sunspot/solr components

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,23 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
 
   searchable do
-    text :title, :body
-    text :comments do
-      comments.map { |comment| comment.body }
-    end
-
-    boolean :featured
-    integer :blog_id
-    integer :author_id
-    integer :category_ids, :multiple => true
-    double  :average_rating
-    time    :published_at
-    time    :expired_at
-
-    string  :sort_title do
-      title.downcase.gsub(/^(an?|the)/, '')
-    end
-  end
+  text :body
 end
 
 end
